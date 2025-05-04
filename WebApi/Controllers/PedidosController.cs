74696ba135d12a6f4e8fc6756133ab749cb1cf60
Dissoares <diegosoaresyt@gmail.com>
2025-05-04 19:58:47 -0300

    atualizadano endpoints

--- WebApi/Controllers/PedidosController.cs
+++ WebApi/Controllers/PedidosController.cs
@@ -16,26 +16,40 @@ namespace WebApi.Controllers
         {
             _context = context;
         }
-
-        [HttpGet]
         public async Task<ActionResult<IEnumerable<Pedido>>> BuscarTodosOsPedidos()
         {
-           
+            return await _context.Pedido
+                .Include(p => p.Cliente)
+                .Include(p => p.Produtos)
+                .ToListAsync();
         }
 
-        [HttpGet]
+        [HttpGet("{id}")]
         public async Task<ActionResult<Pedido>> BurcarPedidoPorId(int id)
         {
-            
+            var pedido = await _context.Pedido
+                .Include(p => p.Cliente)
+                .Include(p => p.Produtos)
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+
+            return pedido;
         }
 
         [HttpPost]
         public async Task<ActionResult<Pedido>> AdicionarPedido(Pedido pedido)
         {
-           
+            _context.Pedido.Add(pedido);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(BurcarPedidoPorId), new { id = pedido.Id }, pedido);
         }
 
-       
+
     }
 }
 
