4bc3a2a32de05ed8b3d7a105df1a790a62d1219c
Dissoares <diegosoaresyt@gmail.com>
2025-05-04 18:49:28 -0300

    adinanco endpoints basicos em produtos

--- WebApi/Controllers/ProdutosController.cs
+++ WebApi/Controllers/ProdutosController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Domain.Entities;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApi.Controllers
 {
@@ -7,5 +9,36 @@ namespace WebApi.Controllers
     [ApiController]
     public class ProdutosController : ControllerBase
     {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Adicionar(Produto produto)
+        {
+            _context.Produto.Add(produto);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(BuscarPorId), new { id = produto.Id }, produto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BuscarTodos()
+        {
+            var produtos = await _context.Produto.ToListAsync();
+            return Ok(produtos);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> BuscarPorId(int id)
+        {
+            var produto = await _context.Produto.FindAsync(id);
+            if (produto == null) return NotFound();
+            return Ok(produto);
+        }
+
+
     }
 }
